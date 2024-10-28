c5b4141609b138ea2dd79097264117e2340291d2
sergioliveira-developer <sergiowallace11@hotmail.com>
2024-10-28 00:15:16 +0000

    vercel error build

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,9 +2,9 @@ import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 
 //components
-import Header from '@/components/Header';
 import PageTransition from '@/components/PageTransition';
 import StairTransition from '@/components/StairTransition';
+import Header from '@/components/Header';
 
 const jetBrainsMono = JetBrains_Mono({
   subsets: ['latin'],
